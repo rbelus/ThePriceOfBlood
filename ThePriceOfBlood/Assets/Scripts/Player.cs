4a7e3095d7960fe10bb9fbefbf67b134e5e1d852
Unknown <raphael.belus@yahoo.com>
2016-11-05 16:32:45 +0100

    Version 0.000006
    Added : LifeBar UI
    Life lose with Time

--- ThePriceOfBlood/Assets/Scripts/Player.cs
+++ ThePriceOfBlood/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
@@ -26,6 +27,12 @@ public class Player : MonoBehaviour {
     // 0 - VIE, 1 - ATK, 2 - SPD, 3 - REGEN, 4 - ANTIREGEN, 5 - DEF
     public int[] mStats;
 
+    // Vie actuelle
+    public float mCurrentLife;
+
+    // UI
+    public Slider mLifeSlider;
+
     void Start()
     {
         mStats = new int[6];
@@ -40,10 +47,12 @@ public class Player : MonoBehaviour {
         mLook = Vector3.back;
         mAnimator = GetComponent<Animator>();
         mSpriteRenderer = GetComponent<SpriteRenderer>();
+        mCurrentLife = mStats[0];
     }
 
     void Update()
     {
+        // GESTION DES DEPLACEMENTS ET DES ACTIONS 
         switch (mCurrentState)
         {
             case CharacterState.IDLE:
@@ -117,7 +126,18 @@ public class Player : MonoBehaviour {
             Debug.Log("Collision");
             mSpeed = Vector3.zero;
         }
- 
+
+        // Affichage de la vie
+        mLifeSlider.value = mCurrentLife / mStats[0];
+
+        // Perte de vie
+        mCurrentLife -= 0.3f * Time.deltaTime;
+
+        // DID YOU JUST DIE ? BITCH
+        if (mCurrentLife <= 0)
+            Destroy(this.gameObject);
+
+        // Update Position
         mPosition += mSpeed * Time.deltaTime;
         transform.position = mPosition;
     }
