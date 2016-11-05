35cb1bf088e316d70f4914f196c486b876098680
Unknown <raphael.belus@yahoo.com>
2016-11-05 12:39:15 +0100

    Version 0.000003
    Added : animation

--- ThePriceOfBlood/Assets/Scripts/Player.cs
+++ ThePriceOfBlood/Assets/Scripts/Player.cs
@@ -15,11 +15,17 @@ public class Player : MonoBehaviour {
     // Regard
     public Vector3 mLook;
 
+    // Animator
+    private Animator mAnimator;
+    private SpriteRenderer mSpriteRenderer;
+
     void Start()
     {
         mCurrentState = CharacterState.IDLE;
         mPosition = transform.position;
         mLook = Vector3.back;
+        mAnimator = GetComponent<Animator>();
+        mSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -46,11 +52,17 @@ public class Player : MonoBehaviour {
                 }
             case CharacterState.MOVE:
                 {
+                    mAnimator.SetBool("playerMoveBool", true);
                     if (System.Math.Abs(Input.GetAxis("Horizontal")) > 0.5 ||
                             System.Math.Abs(Input.GetAxis("Vertical")) > 0.5)
                     {
                         mSpeed.x = Input.GetAxis("Horizontal");
                         mSpeed.z = Input.GetAxis("Vertical");
+
+                        if (mSpeed.x < 0)
+                            mSpriteRenderer.flipX = true;
+                        else
+                            mSpriteRenderer.flipX = false;
 
                         mSpeed.Normalize();
                         mLook = mSpeed;
@@ -60,6 +72,7 @@ public class Player : MonoBehaviour {
                     {
                         mSpeed = Vector3.zero;
                         mCurrentState = CharacterState.IDLE;
+                        mAnimator.SetBool("playerMoveBool", false);
                         break;
                     }
 
@@ -74,8 +87,5 @@ public class Player : MonoBehaviour {
 
         mPosition += mSpeed * Time.deltaTime;
         transform.position = mPosition;
-
-        Debug.Log("Axis value : " + Input.GetAxis("Vertical"));
-
     }
 }
